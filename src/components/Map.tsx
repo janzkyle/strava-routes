b8a9ef76f1fb14c134b77b6e06bc1bfa54567c3a
Kyle Ordoña <janzrelkyleordona@gmail.com>
2021-07-24 22:43:05 +0800

    setup prettier, husky and lint-staged

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import DeckGL from '@deck.gl/react';
-import {StaticMap, GeolocateControl} from 'react-map-gl';
+import React from "react";
+import DeckGL from "@deck.gl/react";
+import { StaticMap, GeolocateControl } from "react-map-gl";
 
 type View = {
   latitude: number;
@@ -8,31 +8,28 @@ type View = {
   zoom: number;
   pitch: number;
   bearing: number;
-}
+};
 
 const INITIAL_VIEW_STATE: View = {
   longitude: -122.41669,
   latitude: 37.7853,
   zoom: 13,
   pitch: 0,
-  bearing: 0
+  bearing: 0,
 };
 
-const geolocateControlStyle= {
+const geolocateControlStyle = {
   right: 10,
-  top: 10
+  top: 10,
 };
 
 function Map() {
   return (
-    <DeckGL
-      initialViewState={INITIAL_VIEW_STATE}
-      controller={true}
-    >
+    <DeckGL initialViewState={INITIAL_VIEW_STATE} controller={true}>
       <StaticMap mapStyle="mapbox://styles/mapbox/outdoors-v11">
         <GeolocateControl
           style={geolocateControlStyle}
-          positionOptions={{enableHighAccuracy: true}}
+          positionOptions={{ enableHighAccuracy: true }}
           trackUserLocation={true}
           auto
         />
