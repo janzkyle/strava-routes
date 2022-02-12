b09a7a9ef2110ce16ea500b1e2c895f9c85326ea
Kyle Ordoña <janzrelkyleordona@gmail.com>
2022-02-12 23:14:22 +0800

    set access token as cookie

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import Login from "./pages/Login";
 import { useLocation } from "react-router-dom";
 import { stravaAPI } from "./api";
 import { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
 
 function App() {
+  const [cookies, setCookie] = useCookies();
+
   let location = useLocation();
   const search = location?.search;
   const code = new URLSearchParams(search).get("code");
@@ -13,7 +16,13 @@ function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   useEffect(() => {
-    if (code && scope?.includes("activity:read_all")) {
+    if (cookies["access_token"]) {
+      stravaAPI.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${cookies["access_token"]}`;
+
+      setIsAuthenticated(true);
+    } else if (code && scope?.includes("activity:read_all")) {
       (async function fetchToken() {
         const tokens = await stravaAPI.post("/oauth/token", {
           client_id: parseInt(process.env.REACT_APP_STRAVA_CLIENT_ID!, 10),
@@ -25,10 +34,12 @@ function App() {
         stravaAPI.defaults.headers.common[
           "Authorization"
         ] = `Bearer ${tokens.data.access_token}`;
+
+        setCookie("access_token", tokens.data.access_token);
         setIsAuthenticated(true);
       })();
     }
-  }, [code, scope]);
+  }, [code, cookies, scope, setCookie]);
 
   return isAuthenticated ? <Map /> : <Login />;
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
+import { CookiesProvider } from "react-cookie";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <CookiesProvider>
+        <App />
+      </CookiesProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
